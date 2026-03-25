7dc95e87884e33a2930ce57a0b68f615abc06c9f
Abs Salim <abdurahe@ualberta.ca>
2026-03-25 12:47:16 -0600

    updated cmakes and removed password handling

--- server/include/encryption.h
+++ server/include/encryption.h
@@ -7,12 +7,4 @@
 // populates a buffer with cryptographically random bytes
 void populate_cryptorandom(char* buf, size_t size);
 
-// Hashes a user's password to be used for storing in the database
-char* generate_password_hash(char* password);
-
-// Verifies a user's password against the hashed password
-// 
-// returns 1 if true, 0 if false
-int verify_password(char* hashed_password, char* user_input);
-
 #endif
